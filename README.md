db3a30a5bd3c40ad885d2c09549faaa82d451128
Ahmed Mustafa <ahmed.akzm@gmail.com>
2014-10-26 21:37:11 +0300

    Implemented basic authentication for sockets

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Chess Online
 This is a simple online chess platform implemented with the MEAN stack. I will continuously update this document as I make more progress.
  
-## Currently implemented features
- * Basic user authentication via the Passport package.
+## Progress so far
+ * Basic user authentication (HTTP) via the Passport package.
+ * Basic user authentication (Sockets.io) via the Passport package.
+ * Simple mapping of {user.id => socket}.
+
+### Notes
+ * Currently session is being managed in main memory via express-session's default session store. I intend to configure a persistent memory store (most likely Redis), mainly for scalability and robustness.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // modules ===================================================================
 var express        = require('express');
 var app            = express();
+var socketIO       = require('socket.io');
 var bodyParser     = require('body-parser');
 var cookieParser   = require('cookie-parser');
 var methodOverride = require('method-override');
@@ -17,56 +18,87 @@ mongoose.connect(db.url);
 
 // configure passport ========================================================
 authentication(passport);
+var passportInitialize = passport.initialize();
+var passportSession = passport.session();
 
 
-// middleware ================================================================
-SECRET_KEY = 'SOME_SECRET_KEY'; // In production, this won't be stored in the 
-                                // repository. I will most likely use an 
-                                // environment variable to set this variable.
+// set up express session ====================================================
+var sessionMiddleware = expressSession({
+    secret: 'SOME_SECRET_KEY', // This will become an environment variable later
+    saveUninitialized: true,
+    resave: true
+});
 
+
+// express middleware ========================================================
 app.use(express.static(__dirname + '/public'));
-app.use(cookieParser()); // *** Should this use the SECRET_KEY too? ***
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(expressSession({secret: SECRET_KEY, saveUninitialized: true, resave: true}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(sessionMiddleware);
+app.use(passportInitialize);
+app.use(passportSession);
 
 
-// routes ====================================================================
+// configure routes ==========================================================
 //require('./app/routes')(app); // configure our routes 
 app.get('/index', function(req, res) {
-    console.log("\n==============");
-    console.log(req.user);
-    console.log(req.session);
     res.sendfile('./public/views/index.html');
 });
 
 app.get('/failed', function(req, res) {
-    console.log("\n==============");
-    console.log(req.user);
-    console.log(req.session);
     res.send("AUTHENTICATION FAILED ::: " + req.isAuthenticated());
 });
 
 app.get('/success', function(req, res) {
-    console.log("\n==============");
-    console.log(req.user);
-    console.log(req.session);
     res.send("AUTHENTICATION SUCCEEDED ::: " + req.isAuthenticated());
 });
  
 app.post('/login',
-    passport.authenticate('local', { successRedirect: '/success',
+    passport.authenticate('local', { successRedirect: '/index',
                                      failureRedirect: '/failed' })
 );
 
 
 // start server ==============================================================
-app.listen(process.env.PORT || 8080);
+var httpServer = app.listen(process.env.PORT || 8080);
 
 
+// set up socket.io ==========================================================
+var sio = socketIO(httpServer);
+var activeSockets = {/* maps user.id --> socket */};
+
+sio.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});
+
+sio.use(function(socket, next) {
+    passportInitialize(socket.request, socket.request.res, next);
+});
+
+sio.use(function(socket, next) {
+    passportSession(socket.request, socket.request.res, next);
+});
+
+sio.use(function(socket, next) {
+    if(socket.request.isAuthenticated()) {
+        next();
+    } else {
+        next(new Error("unauthenticated session"));
+    }
+});
+
+sio.on("connection", function(socket) {
+    activeSockets[socket.request.user.id] = socket;
+
+    socket.on('disconnect', function() {
+        delete activeSockets[socket.request.user.id];
+        console.error(socket.request.user.id + " has disconnected!");
+    });
+
+    // Implement other socket activity here
+});
 
 
 // random stuff (development only - this will be implemented formally) =======
@@ -77,5 +109,13 @@ new User({
     last_name: "mustafa",
     username: "ahmedakzm",
     email: "ahmed.akzm@gmail.com",
+    password: "ahmedahmed"
+}).save(function(err) {});
+
+new User({
+    first_name: "ahmed",
+    last_name: "mustafa",
+    username: "ahmedakzm2",
+    email: "ahmed.akzm2@gmail.com",
     password: "ahmedahmed"
 }).save(function(err) {});
